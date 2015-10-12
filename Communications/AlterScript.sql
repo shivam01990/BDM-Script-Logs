5003d79b8ce9da59b02636d4d455430ecf51f595
Shivam Srivastava <shivam01990@gmail.com>
2015-10-12 16:53:55 +0530

    Procedure List

--- Communications/AlterScript.sql
+++ Communications/AlterScript.sql
@@ -2,3 +2,6 @@ Alter Table  bdm.NewsLetterTemplates
 Add OfferType smallint
 
 Update bdm.NewsLetterTemplates SET OfferType =1
+
+ALTER TABLE bdm.NewsLetterTemplates
+ ALTER COLUMN OfferType smallint Not NULL
